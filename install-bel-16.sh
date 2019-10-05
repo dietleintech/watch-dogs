2a0a8dba8db8823045168a18fb5de0cdb1e3e565
Mario Dietlein <mario@dietlein.tech>
2019-10-05 17:54:31 +0200

    Delete install-bel-16.sh

--- install-bel-16.sh
+++
@@ -1,5 +0,0 @@
-sudo update-alternatives --install /lib/plymouth/themes/default.plymouth default.plymouth /lib/plymouth/themes/watch-dogs/watch-dogs.plymouth 100
-
-sudo update-alternatives --config default.plymouth
-
-sudo update-initramfs -u
